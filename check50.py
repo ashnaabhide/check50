1212fe28b8e17609102f4a31a2808036ecc43ebf
Brian Yu <brianyu28@gmail.com>
2017-08-01 18:58:03 -0400

    Add debugging statements

--- check50.py
+++ check50.py
@@ -40,6 +40,7 @@ __all__ = ["check", "Checks", "Child", "EOF", "Error", "File", "Mismatch", "valg
 def main():
 
     # parse command line arguments
+    print("about to parse arguments...", file=sys.stderr)
     parser = argparse.ArgumentParser()
     parser.add_argument("identifier", nargs=1)
     parser.add_argument("files", nargs="*")
@@ -116,6 +117,7 @@ def main():
             sys.exit(0)
 
     # copy all files to temporary directory
+    print("about to copy files to temporary dir...", file=sys.stderr)
     config.tempdir = tempfile.mkdtemp()
     src_dir = os.path.join(config.tempdir, "_")
     os.mkdir(src_dir)
@@ -123,46 +125,60 @@ def main():
         files = os.listdir(".")
     for filename in files:
         copy(filename, src_dir)
+    print("done copying files to temporary dir...", file=sys.stderr)
 
+    print("about to import checks...", file=sys.stderr)
     checks = import_checks(identifier)
+    print("done with import checks...", file=sys.stderr)
 
     # create and run the test suite
+    print("about to create suite...", file=sys.stderr)
     suite = unittest.TestSuite()
     for case in config.test_cases:
         suite.addTest(checks(case))
     result = TestResult()
+    print("about to run suite...", file=sys.stderr)
     suite.run(result)
+    print("about to cleanup...", file=sys.stderr)
     cleanup()
+    print("done with cleanup...", file=sys.stderr)
 
     # Get list of results from TestResult class
     results = result.results
 
     # print the results
+    print("about to print results...", file=sys.stderr)
     if config.args.debug:
         print_json(results)
     else:
         print_results(results, log=config.args.log)
+    print("done...", file=sys.stderr)
 
 
 @contextmanager
 def cd(path):
     """can be used with a `with` statement to temporarily change directories"""
+    print("start cd...", file=sys.stderr)
     cwd = os.getcwd()
     try:
         os.chdir(path)
         yield
     finally:
         os.chdir(cwd)
+    print("end cd...", file=sys.stderr)
 
 
 def cleanup():
     """Remove temporary files at end of test."""
+    print("start cleanup...", file=sys.stderr)
     if config.tempdir:
         shutil.rmtree(config.tempdir)
+    print("end cleanup...", file=sys.stderr)
 
 
 def copy(src, dst):
     """Copy src to dst, copying recursively if src is a directory"""
+    print("start copy...", file=sys.stderr)
     try:
         shutil.copytree(src, os.path.join(dst, os.path.basename(src)))
     except (OSError, IOError) as e:
@@ -170,9 +186,11 @@ def copy(src, dst):
             shutil.copy(src, dst)
         else:
             raise
+    print("end copy...", file=sys.stderr)
 
 
 def excepthook(cls, exc, tb):
+    print("excepthook...", file=sys.stderr)
     cleanup()
 
     # Class is a BaseException, better just quit
@@ -195,6 +213,7 @@ sys.excepthook = excepthook
 
 
 def print_results(results, log=False):
+    print("about to start printing results...", file=sys.stderr)
     for result in results:
         if result["status"] == Checks.PASS:
             cprint(":) {}".format(result["description"]), "green")
@@ -209,10 +228,12 @@ def print_results(results, log=False):
         if log:
             for line in result["test"].log:
                 print("    {}".format(line))
+    print("done with printing results...", file=sys.stderr)
 
 
 def print_json(results):
     output = []
+    print("about to iterate through results...", file=sys.stderr)
     for result in results:
         obj = {
             "name": result["test"]._testMethodName,
@@ -232,6 +253,7 @@ def print_json(results):
             pass
 
         output.append(obj)
+    print("about to print results...", file=sys.stderr)
     print(json.dumps(output))
 
 
@@ -244,38 +266,50 @@ def import_checks(identifier):
 
     Throws ImportError on error
     """
+    print("start import checks...", file=sys.stderr)
     try:
+        print("trying to split at @...", file=sys.stderr)
         slug, repo = identifier.split("@")
     except ValueError:
+        print("got exception, assuming cs50/checks...", file=sys.stderr)
         slug, repo = identifier, "cs50/checks"
 
     try:
+        print("trying to split repo at /...", file=sys.stderr)
         org, repo = repo.split("/")
     except ValueError:
+        print("repo not well formatted...", file=sys.stderr)
         raise InternalError("expected repository to be of the form username/repository, but got \"{}\"".format(repo))
 
-
+    print("determining directories...", file=sys.stderr)
     checks_root = os.path.join(config.args.checkdir, org, repo)
     config.check_dir = os.path.join(checks_root, slug.replace("/", os.sep), "check50")
 
+    print("setting up command for clone or pulling", file=sys.stderr)
     if not config.args.offline:
         if os.path.exists(checks_root):
+            print("going to pull", file=sys.stderr)
             command = ["git", "-C", checks_root, "pull", "origin", "master"]
 
         else:
+            print("going to clone...", file=sys.stderr)
             command = ["git", "clone", "https://github.com/{}/{}".format(org, repo), checks_root]
 
         # Can't use subprocess.DEVNULL because it requires python 3.3
+        print("redirecting stdout and stderr...", file=sys.stderr)
         stdout = stderr = None if config.args.verbose else open(os.devnull, "wb")
 
         # Update checks via git
         try:
+            print("about to make call to clone or check...", file=sys.stderr)
             subprocess.check_call(command, stdout=stdout, stderr=stderr)
+            print("done with call to clone or check...", file=sys.stderr)
         except subprocess.CalledProcessError:
             raise InternalError("failed to clone checks")
 
 
     # Install any dependencies from requirements.txt either in the root of the repository or in the directory of the specific check
+    print("about to search for dependencies...", file=sys.stderr)
     for dir in [checks_root, os.path.dirname(config.check_dir)]:
         requirements = os.path.join(dir, "requirements.txt")
         if os.path.exists(requirements):
@@ -294,11 +328,15 @@ def import_checks(identifier):
 
             if code:
                 raise InternalError("failed to install dependencies in ({})".format(requirements[len(config.args.checkdir)+1:]))
+    print("done with search for dependencies...", file=sys.stderr)
 
     try:
         # Import module from file path directly
+        print("about to try to load the module...", file=sys.stderr)
         module = imp.load_source(slug, os.path.join(config.check_dir, "__init__.py"))
+        print("loaded the module...", file=sys.stderr)
         # Ensure that there is exactly one class decending from Checks defined in this package
+        print("ensuring that checks exist...", file=sys.stderr)
         checks, = (cls for _, cls in inspect.getmembers(module, inspect.isclass)
                        if hasattr(cls, "_Checks__sentinel")
                            and cls.__module__.startswith(slug))
@@ -328,6 +366,7 @@ class TestResult(unittest.TestResult):
 
     def addSuccess(self, test):
         """Handle completion of test, regardless of outcome."""
+        print("got a successful check: {} with result {}...".format(test.shortDescription(), test.result), file=sys.stderr)
         self.results.append({
             "description": test.shortDescription(),
             "helpers": test.helpers,
@@ -338,6 +377,7 @@ class TestResult(unittest.TestResult):
         })
 
     def addError(self, test, err):
+        print("got an error...", file=sys.stderr)
         self.results.append({
             "description": test.shortDescription(),
             "helpers": test.helpers,
@@ -382,21 +422,26 @@ def check(dependency=None):
         def wrapper(self):
 
             # check if dependency failed
+            print("running check {}".format(func.__name__), file=sys.stderr)
+            print("checking if dependency is satisfied...", file=sys.stderr)
             if dependency and config.test_results.get(dependency) != Checks.PASS:
                 self.result = config.test_results[func.__name__] = Checks.SKIP
                 self.rationale = "can't check until a frown turns upside down"
                 return
 
             # move files into this check's directory
+            print("moving files into this check dir...", file=sys.stderr)
             self.dir = dst_dir = os.path.join(config.tempdir, self._testMethodName)
             src_dir = os.path.join(config.tempdir, dependency or "_")
             shutil.copytree(src_dir, dst_dir)
 
             os.chdir(self.dir)
             # run the test, catch failures
+            print("trying to run test and check failures...", file=sys.stderr)
             try:
                 func(self)
             except Error as e:
+                print("caught an exception...", file=sys.stderr)
                 self.rationale = e.rationale
                 self.helpers = e.helpers
                 result = e.result
@@ -435,6 +480,7 @@ class Child(object):
         self.exitstatus = None
 
     def stdin(self, line, prompt=True, timeout=3):
+        print("stdin...", file=sys.stderr)
         if line == EOF:
             self.test.log.append("sending EOF...")
         else:
@@ -442,6 +488,7 @@ class Child(object):
 
         if prompt:
             try:
+                print("expecting a prompt on stdin...", file=sys.stderr)
                 self.child.expect(".+", timeout=timeout)
             except TIMEOUT:
                 raise Error("expected prompt for input, found none")
@@ -453,11 +500,14 @@ class Child(object):
         return self
 
     def stdout(self, output=None, str_output=None, timeout=3):
+        print("stdout...", file=sys.stderr)
         if output is None:
+            print("in stdout, going to wait and check output...", file=sys.stderr)
             return self.wait(timeout).output
 
         # Files should be interpreted literally, anything else shouldn't be
         try:
+            print("trying to read output...", file=sys.stderr)
             output = output.read()
         except AttributeError:
             expect = self.child.expect
@@ -475,7 +525,9 @@ class Child(object):
         self.test.log.append("checking for output \"{}\"...".format(str_output))
 
         try:
+            print("trying to expect desired output...", file=sys.stderr)
             expect(output, timeout=timeout)
+            print("done with expecting output...", file=sys.stderr)
         except EOF:
             result = self.child.before + self.child.buffer
             if self.child.after != EOF:
@@ -491,9 +543,12 @@ class Child(object):
         return self
 
     def reject(self, timeout=3):
+        print("checking that input was rejected...", file=sys.stderr)
         self.test.log.append("checking that input was rejected...")
         try:
+            print("about to pexpect in reject...", file=sys.stderr)
             self.child.expect(".+", timeout=timeout)
+            print("done with pexpect in reject...", file=sys.stderr)
             self.child.sendline("")
         except OSError:
             self.test.fail()
@@ -502,7 +557,9 @@ class Child(object):
         return self
 
     def exit(self, code=None, timeout=3):
+        print("exit: about to wait...", file=sys.stderr)
         self.wait(timeout)
+        print("exit: done with wait...", file=sys.stderr)
 
         if code is None:
             return self.exitstatus
@@ -513,6 +570,7 @@ class Child(object):
         return self
 
     def wait(self, timeout=3):
+        print("waiting: about to start waiting...", file=sys.stderr)
         end = time.time() + timeout
         while time.time() <= end:
             if not self.child.isalive():
@@ -527,8 +585,10 @@ class Child(object):
                 self.output.append(bytes)
         else:
             raise Error("timed out while waiting for program to exit")
+        print("done waiting", file=sys.stderr)
 
         # Read any remaining data in pipe
+        print("about to read in other bytes", file=sys.stderr)
         while True:
             try:
                 bytes = self.child.read_nonblocking(size=1024, timeout=0)
@@ -536,6 +596,7 @@ class Child(object):
                 break
             else:
                 self.output.append(bytes)
+        print("done with reading in other bytes", file=sys.stderr)
 
         self.output = "".join(self.output).replace("\r\n", "\n").lstrip("\n")
         self.kill()
@@ -581,10 +642,12 @@ class Checks(unittest.TestCase):
 
     def require(self, *paths):
         """Asserts that all paths exist."""
+        print("starting require", file=sys.stderr)
         for path in paths:
             self.log.append("Checking that {} exists...".format(path))
             if not os.path.exists(path):
                 raise Error("{} not found".format(path))
+        print("ending require", file=sys.stderr)
 
     def hash(self, filename):
         """Hashes a file using SHA-256."""
@@ -606,6 +669,7 @@ class Checks(unittest.TestCase):
 
     def spawn(self, cmd, env=None):
         """Spawns a new child process."""
+        print("about to spawn", file=sys.stderr)
         if self._valgrind:
             self.log.append("running valgrind {}...".format(cmd))
             cmd = "valgrind --show-leak-kinds=all --xml=yes --xml-file={} -- {}" \
@@ -623,7 +687,9 @@ class Checks(unittest.TestCase):
         if sys.version_info < (3, 0):
             child = pexpect.spawn(cmd, echo=False, env=env)
         else:
+            print("going to run pexpect.spawn", file=sys.stderr)
             child = pexpect.spawnu(cmd, encoding="utf-8", echo=False, env=env)
+            print("done with run pexpect.spawn", file=sys.stderr)
 
         self.children.append(Child(self, child))
         return self.children[-1]
